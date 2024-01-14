0b8e4e3b374d78ef08b5e4ef7aafd1e058405799
Hassan BOUDRAA <hassan.hbmama@gmail.com>
2024-01-14 07:59:56 -0800

    add new requests in js for tests all outputs x

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,66 +1,122 @@
 const $ = window.$;
 $(document).ready(function () {
-	const amenityDict = {};
-	$('input[type=checkbox]').change(function () {
-		const amenityId = $(this).data('id');
-		const amenityName = $(this).data('name');
-		if ($(this).prop('checked')) {
-			amenityDict[amenityId] = amenityName;
-		} else {
-			delete amenityDict[amenityId];
-		}
+  $('.amenities h4').html('&nbsp;');
+  $('.locations h4').html('&nbsp;');
+  const amenityDict = {};
+  const stateDict = {};
+  const cityDict = {};
+  $('.amenities input[type=checkbox]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if ($(this).prop('checked')) {
+      amenityDict[amenityId] = amenityName;
+      console.log('Updated amenity dictionary:', amenityDict);
+    } else {
+      delete amenityDict[amenityId];
+    }
+    if (Object.keys(amenityDict).length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(Object.values(amenityDict).join(', '));
+    }
+  });
+  $('.locations h2 input[type=checkbox]').change(function () {
+    const stateId = $(this).data('id');
+    const stateName = $(this).data('name');
+    if ($(this).prop('checked')) {
+      stateDict[stateId] = stateName;
+       console.log('Updated state dictionary:', stateDict);
+    } else {
+      delete stateDict[stateId];
+    }
+    if (Object.keys(stateDict).length === 0) {
+      $('.locations h4').html('&nbsp;');
+    } else {
+      $('.locations h4').text(Object.values(stateDict).join(', '));
+    }
+  });
+  $('.locations li ul input[type=checkbox]').change(function () {
+    const cityId = $(this).data('id');
+    const cityName = $(this).data('name');
+    if ($(this).prop('checked')) {
+      cityDict[cityId] = cityName;
+      cityDict[cityId] = cityName;
+       console.log('Updated city dictionary:', cityDict);
+    } else {
+      delete cityDict[cityId];
+    }
+    if (Object.keys(cityDict).length === 0) {
+      $('.locations h4').html('&nbsp;');
+    } else {
+      $('.locations h4').text(Object.values(cityDict).join(', '));
+    }
+  });
 
-		$('.amenities h4').text(object.value(amenityDict).join(', '));
-	});
-	$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-		if (data.status === 'OK') {
-			$('#api_status').addClass('available');
-		} else {
-			$('#api_status').removeClass('available');
-		}
-	});
-	$.ajax({
-		url: 'http://0.0.0.0:5001/api/v1/places_search/',
-		type: 'POST',
-		Content-Type: 'application/json',
-		data: {}
-		success: function(data) {
-			for (const item of data) {
-				$('section.places').append(
-					'<article>' +
-					'<div class="title_box">' +
-					'<h2>' + item.name + '</h2>'+
-					'<div class="price_by_night">$'+
-					item.price_by_night + 
-					'</div>' +
-					'</div>' +
-					'<div class="information">' +
-					'<div class="max_guest">' +
-					item.max_guest + 'Guest' + (item.max_guest !== 1 ? 's' : '') + '</div>' +
-					'<div class="number_rooms">'+
-					item.number_rooms + 'Bedroom' +
-					(item.number_rooms !== 1 ? 's' : '') + '</div>' +
-					'<div class="number_bathrooms">' + item.number_bathrooms + 'Bathrooms' +
-					(item.number_bethroom !== 1 ? 's' : '') + '</div>' +
-					'</div>' +
-					'<div class="user">' +
-					'</div>' +
-					'<div class="description">' + item.description + '</div>' +
-					'</article>'
-				);
-			}
-		}
-	});
-	$('BUTTON').click(function () {
-		$.ajax({
-			url: api + ':5001/api/v1/places_search/',
-			type: 'POST',
-			data: JSON.stringify({ 'amenities': Object.keys(amenities) }),
-			contentType: 'application/json',
-			dataType: 'json',
-			success: appendPlaces
-		});
-	});
+  function renderPlaces (data) {
+    const placesSection = $('section.places');
+    data.forEach(function (place) {
+      const article = $('<article>');
+      article.append('<div class="title_box">');
+      article.find('.title_box').append('<h2>' + place.name + '</h2>');
+      article.find('.title_box').append('<div class="price_by_night">$' + place.price_by_night + '</div>');
+      article.append('<div class="information">');
+      article.find('.information').append('<div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest != 1 ? 's' : '') + '</div>');
+      article.find('.information').append('<div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's' : '') + '</div>');
+      article.find('.information').append('<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's' : '') + '</div>');
+      article.append('<div class="user">');
+      article.append('<div class="description">' + place.description + '</div>');
+      placesSection.append(article);
+    });
+  }
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      renderPlaces(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus, errorThrown);
+    }
+  });
+  $('button').click(function () {
+    const amenityidsList = Object.keys(amenityDict);
+    const stateidsList = Object.keys(stateDict);
+    const cityidsList = Object.keys(cityDict);
+    const Dict = {};
+    if (amenityidsList){
+    Dict['amenities'] = amenityidsList;
+    }
+    if (stateidsList) {
+      if (cityidsList.length === 0) {
+    Dict['states'] = stateidsList;
+      }
+    }
+    if (cityidsList) {
+      Dict['cities'] = cityidsList;
+    }
 
 
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      contentType: 'application/json',
+      data: JSON.stringify(Dict),
+      success: function (data) {
+         $('section.places').empty();
+        renderPlaces(data);       
+        console.log(data);
+    }
+    });
+
+  });
 });
